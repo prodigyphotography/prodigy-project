82e4fee731cb03d0fc0cc25587cb04a9b059b4c5
prodigyphotography <prodigyphotography98@gmail.com>
2024-10-23 20:54:33 +1030

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 const photos = [
     { title: "Roos Mob", thumb: "images/thumbs/roosmob-thumb.JPG", full: "images/roosmob.JPG" },
     { title: "Butterfly - Seaford Rise, SA", thumb: "images/thumbs/butterfly-thumb.jpg", full: "images/butterfly.jpg" },
-    { title: "Water fall - Ingalla falls in Normanville, SA", thumb: "images/thumbs/waterfall-thumb.JPG", full: "images/waterfall.JPG" },
-    { title: "Rainbow - Seaford Rise, SA", thumb: "images/thumbs/Rainbow-thumb.jpg", full: "images/rainbow.jpg" },
+    { title: "Waterfall - Ingalalla Falls in Normanville, SA", thumb: "images/thumbs/waterfall-thumb.JPG", full: "images/waterfall.JPG" },
+    { title: "Rainbow - Seaford Rise, SA", thumb: "images/thumbs/rainbow-thumb.jpg", full: "images/rainbow.jpg" },
 ];
 
 // Cart Data
