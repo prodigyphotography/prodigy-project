6a872a09c69e61db5e084fe55a8b60bc4281fafe
prodigyphotography <prodigyphotography98@gmail.com>
2024-10-23 16:38:34 +1030

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Photo Data
 const photos = [
-    { title: "Roos Mob", thumb: "images/thumbs/RoosMob-thumb.jpg", full: "images/RoosMob.jpg" },
+    { title: "Roos Mob", thumb: "images/thumbs/roosmob-thumb.jpg", full: "images/roosmob.jpg" },
     { title: "Mountain", thumb: "images/thumbs/housedarren-thumb.jpg", full: "images/housedarren.jpg" },
     { title: "Waterfall", thumb: "images/thumbs/waterfall-thumb.jpg", full: "images/waterfall.jpg" },
     { title: "Forest", thumb: "images/thumbs/forest-thumb.jpg", full: "images/forest.jpg" },
